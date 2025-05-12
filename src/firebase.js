7a7a655413e74677433cac0fd365e623a7b4b950
Ato Arkhurst <atoparkhurst@gmail.com>
2025-05-12 14:39:47 -0400

    Create weekly check in UI

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbF03bX13Peud-WD8BP$6NUWWDs_tK-g",
+  authDomain: "cadence-e3b43.firebaseapp.com",
+  projectId: "cadence-e3b43",
+  storageBucket: "cadence-e3b43.appspot.com",
+  messagingSenderId: "220565212851",
+  appId: "1:220565212851:web:3f4a151b4e3470e41aac6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// **THIS PART IS CRITICAL**
+export const auth = getAuth(app);
+export const db = getFirestore(app);
